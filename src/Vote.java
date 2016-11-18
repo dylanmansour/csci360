0893354a884622bbaa7861cdfd79a0f37240f787
Dylan <mansourd@g.cofc.edu>
2016-11-17 20:50:09 -0500

    Update Vote.java

--- src/Vote.java
+++ src/Vote.java
@@ -5,4 +5,24 @@
 public class Vote {
 	String candidate; // the candidate that the vote is for
 	String voterID; // the generated ID of the voter who submitted this vote
+	
+	public String getName()
+	{
+	   return candidate;
+	}
+	  
+	public String getID()
+	{
+	   return voterID;
+	}
+	
+	public void setName(String name)
+	{
+	   this.candidate = name;
+	}
+	  
+	public void setID(String ID)
+	{
+	   this.voterID = ID;
+	}
 }
